f2c82078accd5fa6e07d8c937c6a338b2f92ecd0
stefanotriv <s.trivini@nanogune.eu>
2023-09-18 12:51:38 +0200

    useful mod

--- useful.py
+++ useful.py
@@ -1,10 +1,13 @@
+import matplotlib.pyplot as plt
+import pickle
+import time
+import numpy as np
 #to get a list of measurement filenames given:
 # path, staticname eg.: 'S211026_', indexes: (1,200) and estension (.dat default) 
 def getfnames(path,staticname, idx,extension='.dat'):
-    idxn = [f"{i:03}" for i in range(idx[0],idx[1]+1)]
     fnames = []
-    for idx in idxn:
-        fnames.append(path + staticname + idx + extension)
+    for i in idx:
+        fnames.append(path + staticname + '{:03}'.format(i) + extension)
     return fnames
 
 
@@ -31,3 +34,81 @@ def avg_cond_decon(filenames):
         conductance_avg = conductance_avg + spectra.conductance
     conductance_avg = conductance_avg/len(filenames)
     return bias,conductance_avg/conductance_avg[0]
+
+
+def set_size_cm(w,h, ax=None):
+    """ w, h: width, height in cm """
+    cm = 1/2.54
+    if not ax: ax=plt.gca()
+    l = ax.figure.subplotpars.left
+    r = ax.figure.subplotpars.right
+    t = ax.figure.subplotpars.top
+    b = ax.figure.subplotpars.bottom
+    figw = float(w*cm)/(r-l)
+    figh = float(h*cm)/(t-b)
+    ax.figure.set_size_inches(figw, figh)
+
+def set_size(w,h, ax=None):
+    """ w, h: width, height in inches """
+    if not ax: ax=plt.gca()
+    l = ax.figure.subplotpars.left
+    r = ax.figure.subplotpars.right
+    t = ax.figure.subplotpars.top
+    b = ax.figure.subplotpars.bottom
+    figw = float(w)/(r-l)
+    figh = float(h)/(t-b)
+    ax.figure.set_size_inches(figw, figh)
+
+def save_obj(obj, name ):
+    with open(name + '.pkl', 'wb') as f:
+        pickle.dump(obj, f, pickle.HIGHEST_PROTOCOL) 
+def load_obj(name ):
+    with open( name + '.pkl', 'rb') as f:
+        return pickle.load(f)
+    
+
+def didv(axs):
+    if type(axs) == type(np.zeros(2)):
+        for ax in axs:
+            ax.set_xlabel('Bias (mV)',fontname="Arial")
+            ax.set_ylabel('dI/dV '+r'(G$_N$)',fontname="Arial")
+            ax.tick_params(axis='both',direction='in')
+    else:
+        axs.set_xlabel('Bias (mV)',fontname="Arial")
+        axs.set_ylabel('dI/dV '+r'(G$_N$)',fontname="Arial")
+        axs.tick_params(axis='both',direction='in')
+
+def didv_dec(axs):
+    if type(axs) == type(np.zeros(2)):
+        for ax in axs:
+            ax.set_xlabel('Bias (mV)')
+            ax.set_ylabel('dI/dV dec. '+r'(G$_N$)')
+            ax.tick_params(axis='both',direction='in')
+    else:
+        axs.set_xlabel('E-E'+r'$_F$ (meV)')
+        axs.set_ylabel('dI/dV dec. '+r'(G$_N$)')
+        axs.tick_params(axis='both',direction='in')
+
+def inner(axs):
+    if type(axs) == type(np.zeros(2)):
+        for axn in axs:
+            axn.tick_params(axis='x', direction='in')
+            axn.tick_params(axis='y', direction='in')
+    else:
+            axs.tick_params(axis='x', direction='in')
+            axs.tick_params(axis='y', direction='in')
+
+def timing(i,t0,cycles):
+    if i == 1:
+        #to predict job time
+        t1 = time.time()
+        cycle = (t1-t0)/60
+        total = np.round(cycle*cycles,3)
+        if total <1:
+            print('1 cycle:',np.round(cycle,3),'minutes --- Total time:',np.round(total*60,2),'seconds')
+        else:
+            print('1 cycle:',np.round(cycle,3),'minutes --- Total time:',total,'minutes')
+
+def energyFind(bias, energy):
+    index = (abs(bias - energy)).argmin()
+    return index
