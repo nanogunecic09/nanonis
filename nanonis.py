20986f65870987cd5e52ff77f4e620a7c65e5430
Jon <jon.ortuzar.a@gmail.com>
2024-01-12 11:16:16 +0100

    up

--- nanonis.py
+++ nanonis.py
@@ -396,6 +396,10 @@ class biasSpectroscopy():
             self.biasVI_b = self.data['Bias_VI (V)']
         if 'Bias_VI [bwd] (V)' in self.data:
             self.biasVI_f = self.data['Bias_VI [bwd] (V)']
+        if 'Input 2 (V)' in self.data:
+            self.biasVI_b = self.data['Input 2 (V)']
+        if 'Input 2 [bwd] (V)' in self.data:
+            self.biasVI_f = self.data['Input 2 [bwd] (V)']
     def biasOffset(self, offset):
         self.data['Bias calc (V)'] = self.data['Bias calc (V)']-offset
 
